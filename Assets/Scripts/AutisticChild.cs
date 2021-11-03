5c85b67020b843f055fe96c8c5ae849a0430875c
phgrund <paulinhogrund@gmail.com>
2021-11-03 20:02:39 -0300

    add attach object to autistic hand

--- Assets/Scripts/AutisticChild.cs
+++ Assets/Scripts/AutisticChild.cs
@@ -9,6 +9,16 @@ public class AutisticChild : MonoBehaviour
     public UnityEvent OnCheckpointReached;
     public float turnSpeed = 1f;
 
+    public Transform leftHand;
+    private Transform leftHandTransform;
+    private GameObject leftHandHold = null;
+    private Transform oldLeftHandParent;
+
+    public Transform rightHand;
+    private Transform rightHandTransform;
+    private GameObject rightHandHold = null;
+    private Transform oldRightHandParent;
+
     private NavMeshAgent agent;
     private Animator animator;
     private bool isNavigating = false;
@@ -19,12 +29,14 @@ public class AutisticChild : MonoBehaviour
     void Awake()
     {
         IsWalkingHash = Animator.StringToHash("IsWalking");
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-        animator = GetComponent<Animator>();
+        leftHandTransform = leftHand.Find("Attach") ?? leftHand.transform;
+        rightHandTransform = rightHand.Find("Attach") ?? rightHand.transform;
     }
 
     void Update()
@@ -42,7 +54,7 @@ public class AutisticChild : MonoBehaviour
         }
 
         animator.SetBool(IsWalkingHash, isNavigating);
-        Debug.Log(isNavigating);
+        // Debug.Log(isNavigating);
     }
 
     public void FollowCheckpoint(Checkpoint checkpoint)
@@ -50,6 +62,47 @@ public class AutisticChild : MonoBehaviour
         isNavigating = true;
         agent.SetDestination(checkpoint.transform.position);
         currentCheckpoint = checkpoint;
+    }
+
+    public void AttachObjectToLeftHand(GameObject obj)
+    {
+        DropLeftHandItem();
+        oldLeftHandParent = obj.transform.parent;
+        AttachObjectToHand(leftHandTransform, obj);
+    }
+
+    private void DropLeftHandItem()
+    {
+        if (leftHandHold == null) return;
+        leftHandHold.transform.parent = oldLeftHandParent;
+        oldLeftHandParent = null;
+    }
+
+    public void AttachObjectToRightHand(GameObject obj)
+    {
+        DropRightHandItem();
+        oldRightHandParent = obj.transform.parent;
+        AttachObjectToHand(rightHandTransform, obj);
+    }
+
+    private void DropRightHandItem()
+    {
+        if (rightHandHold == null) return;
+        rightHandHold.transform.parent = oldRightHandParent;
+        oldLeftHandParent = null;
+    }
+
+    private void AttachObjectToHand(Transform hand, GameObject obj)
+    {
+        Rigidbody rigidbody = obj.GetComponent<Rigidbody>();
+        if (rigidbody != null)
+        {
+            rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+        }
+        // Collider collider = obj.GetComponent<Collider>();
+        // if (collider != null) collider.isTrigger = true;
+        obj.transform.parent = hand;
+        obj.transform.position = hand.transform.position;
     }
 
     public void LookAt(GameObject other)
